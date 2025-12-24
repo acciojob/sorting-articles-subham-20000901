5d9a7f1a03b4107096dfb3e7375806ba1878bca5
subham-20000901 <sinhasubh2000@gmail.com>
2025-12-24 18:30:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+const bands =  ['The Plot in You',
+				'The Devil Wears Prada',
+				'Pierce the Veil',
+				'Norma Jean',
+				'The Bled',
+				'Say Anything',
+				'The Midway State',
+				'We Came as Roma',
+				'Counterparts',
+				'Oh, Sleeper',
+				'A Skylit Drive',
+	           'Anywhere But Here',
+				'An Old Dog'];
+
+function remove (ch){
+	return ch.replace(/^(a,an,the)/i,"");
+}
+
+bands.sort((a,b) => {
+	let s1 = remove(a).toLowerCase();
+	let s2 = remove(a).toLowerCase();
+	if(s1<s2) return -1;
+	else if(s1>s2) return 1;
+	return 0;
+})
+
+const ul = document.getElementById("bands");
+
+for(let i=0; i<bands.length; i++){
+	let li = document.createElement("li");
+	li.innertext = bands[i];
+	ul.appendChild(li);
+}
